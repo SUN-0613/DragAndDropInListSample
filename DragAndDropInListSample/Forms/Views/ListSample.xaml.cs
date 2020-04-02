9c33d872aebb3269819399b6b1c1d32659401026
AYamamoto <a-yamamoto@rss-dev.co.jp>
2020-04-02 13:47:19 +0900

    Popupの表示位置を計算せずにxaml指定でマウスポインタにする

--- DragAndDropInListSample/Forms/Views/ListSample.xaml.cs
+++ DragAndDropInListSample/Forms/Views/ListSample.xaml.cs
@@ -12,47 +12,12 @@ namespace DragAndDropInListSample.Forms.Views
     {
 
         /// <summary>
-        /// マウスポインタの座標
-        /// </summary>
-        Point _MousePoint;
-
-        /// <summary>
         /// 一覧表示Sample.View
         /// </summary>
         public ListSample()
         {
 
             InitializeComponent();
-
-        }
-
-        /// <summary>
-        /// ListBox.マウス左ボタン押下
-        /// </summary>
-        /// <param name="sender">ListBox</param>
-        /// <param name="e">マウスボタンイベントデータ</param>
-        private void ListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-
-            _MousePoint = e.GetPosition(this);
-
-        }
-
-        /// <summary>
-        /// Popup.Opend
-        /// </summary>
-        /// <param name="sender">Popup</param>
-        /// <param name="e">イベントデータ</param>
-        private void Popup_Opened(object sender, EventArgs e)
-        {
-
-            if (sender is Popup popup)
-            {
-
-                popup.HorizontalOffset = _MousePoint.X - 10d;
-                popup.VerticalOffset = _MousePoint.Y - 10d;
-
-            }
 
         }
 
