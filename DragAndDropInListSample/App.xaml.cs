071dcdecfc957e9cfe7427d68c6f57923df6cf9b
AYamamoto <a-yamamoto@rss-dev.co.jp>
2020-04-02 10:11:24 +0900

    プロジェクト作成

---
+++ DragAndDropInListSample/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace DragAndDropInListSample
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+
+        /// <summary>
+        /// 処理開始
+        /// </summary>
+        /// <param name="e">コマンドライン引数イベントデータ</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            base.OnStartup(e);
+
+        }
+
+    }
+
+}
