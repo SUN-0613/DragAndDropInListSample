677e45a3d79b9843c2d63527765ba969b9aec2a4
AYamamoto <a-yamamoto@rss-dev.co.jp>
2020-04-02 13:45:41 +0900

    Popupの初期位置を計算してマウスポインタの位置にセット

--- DragAndDropInListSample/Forms/Views/ListSample.xaml.cs
+++ DragAndDropInListSample/Forms/Views/ListSample.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace DragAndDropInListSample.Forms.Views
 {
@@ -9,12 +12,47 @@ namespace DragAndDropInListSample.Forms.Views
     {
 
         /// <summary>
+        /// マウスポインタの座標
+        /// </summary>
+        Point _MousePoint;
+
+        /// <summary>
         /// 一覧表示Sample.View
         /// </summary>
         public ListSample()
         {
 
             InitializeComponent();
+
+        }
+
+        /// <summary>
+        /// ListBox.マウス左ボタン押下
+        /// </summary>
+        /// <param name="sender">ListBox</param>
+        /// <param name="e">マウスボタンイベントデータ</param>
+        private void ListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            _MousePoint = e.GetPosition(this);
+
+        }
+
+        /// <summary>
+        /// Popup.Opend
+        /// </summary>
+        /// <param name="sender">Popup</param>
+        /// <param name="e">イベントデータ</param>
+        private void Popup_Opened(object sender, EventArgs e)
+        {
+
+            if (sender is Popup popup)
+            {
+
+                popup.HorizontalOffset = _MousePoint.X - 10d;
+                popup.VerticalOffset = _MousePoint.Y - 10d;
+
+            }
 
         }
 
